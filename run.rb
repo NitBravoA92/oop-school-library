61a9e947100a8e228cd79540a8b1973d6032e012
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-28 16:43:48 -0400

    Create an object of the Student class to test its functionality

--- run.rb
+++ run.rb
@@ -1,5 +1,12 @@
 require './classes/person'
+require './classes/student'
 
-# create an object with the data of Rick
+# create an object with the data of the person: Rick
 person1 = Person.new(15, 'Rick', parent_permission: true)
 p person1.can_use_services?
+
+# create an object with the data of the student: James
+student1 = Student.new(10, 19, 'James', parent_permission: false)
+p "#{student1.name}, age: #{student1.age}, classroom: #{student1.classroom}"
+p student1.play_hooky
+p student1.can_use_services?
