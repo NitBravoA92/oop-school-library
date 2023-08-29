1a10b91d326340489114c80df02aaa5bd47dc5e3
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-28 22:56:49 -0400

    Create the TrimmerDecorator class that inherits from Decorator

---
+++ classes/trimmer_decorator.rb
@@ -0,0 +1,9 @@
+require_relative 'Decorator'
+
+class TrimmerDecorator < Decorator
+  def correct_name
+    @nameable.correct_name.length <= 10 
+      ? @nameable.correct_name 
+      : @nameable.correct_name[0..9]
+  end
+end
