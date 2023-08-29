821e8836d87046c73c6e66e6cae827b52635dfad
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-28 22:30:14 -0400

    Implement inheritance from Nameable to Person

--- classes/person.rb
+++ classes/person.rb
@@ -1,5 +1,8 @@
-class Person
+require_relative 'nameable'
+
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
+    super()
     @id = Random.rand(1...10_000)
     @age = age
     @name = name
@@ -19,5 +22,9 @@ class Person
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def correct_name
+    @name
   end
 end
