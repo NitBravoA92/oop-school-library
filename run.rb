b26a1bcae1ec0b1053603aa77b7f90a4cc4b08fd
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-28 23:04:17 -0400

    Import the decorators and add code to test their functionalities

--- run.rb
+++ run.rb
@@ -1,6 +1,8 @@
 require './classes/person'
 require './classes/student'
 require './classes/teacher'
+require './classes/capitalize_decorator'
+require './classes/trimmer_decorator'
 
 # create an object with the data of the person: Rick
 person1 = Person.new(15, 'Rick', parent_permission: true)
@@ -16,3 +18,11 @@ p student1.can_use_services?
 teacher1 = Teacher.new('Data Science', 40, 'Henry')
 p "#{teacher1.name}, age: #{teacher1.age}, specialization: #{teacher1.specialization}"
 p teacher1.can_use_services?
+
+# Test the Decorator design pattern implementation
+person2 = Person.new(22, 'maximilianus')
+p person2.correct_name
+capitalized_person2 = CapitalizeDecorator.new(person2)
+p capitalized_person2.correct_name
+capitalized_trimmed_person2 = TrimmerDecorator.new(capitalized_person2)
+p capitalized_trimmed_person2.correct_name
