2b0119c42f9517af63dad6c60efc4af169077dcb
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-28 23:05:15 -0400

    Fix issue importing the decorator file

--- classes/capitalize_decorator.rb
+++ classes/capitalize_decorator.rb
@@ -1,4 +1,4 @@
-require_relative 'Decorator'
+require_relative 'decorator'
 
 class CapitalizeDecorator < Decorator
   def correct_name
--- classes/trimmer_decorator.rb
+++ classes/trimmer_decorator.rb
@@ -1,4 +1,4 @@
-require_relative 'Decorator'
+require_relative 'decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
