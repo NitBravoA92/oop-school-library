c4c8f364ea225c0b1227821355dd5572f0713e02
Carmen Bravo <nbravoalcala@gmail.com>
2023-08-30 18:36:42 -0400

    Create the Classroom class and add a relationship with the Student class

---
+++ classes/classroom.rb
@@ -0,0 +1,14 @@
+class Classroom
+  attr_accessor :label
+  attr_reader :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def enter_student(student)
+    @students.push(student)
+    student.classroom = self # make this student part of this classroom 
+  end
+end
