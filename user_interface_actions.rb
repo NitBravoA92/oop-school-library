846280f1d29acf3885c56e91247e1297f994cb60
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-04 18:23:09 -0400

    Create the display_rentals method in UserInterfaceActions class

--- user_interface_actions.rb
+++ user_interface_actions.rb
@@ -13,4 +13,11 @@ class UserInterfaceActions
     @application.list_people
   end
 
+  def display_rentals
+    print 'ID of person:'
+    person_id = gets.chomp.to_i
+    puts 'Rentals:'
+    @application.list_rentals(person_id)
+  end
+
 end
