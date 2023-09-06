a6086027511885eada2ad5019bd613c53ad67385
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-07 00:44:11 +0300

    added unit test for creating new book

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -6,4 +6,25 @@ describe Book do
   before :each do
     @book = Book.new(1, "The Great Gatsby", "F. Scott Fitzgerald")
   end
+  describe "#new" do
+    it "should be able to be created" do
+      expect(@book).to be_instance_of(Book)
+    end
+
+    it "should save the id of the new book" do
+      expect(@book.id).to eq(1)
+    end
+
+    it "should save the title of the new book" do
+      expect(@book.title).to eq("The Great Gatsby")
+    end
+
+    it "should save the author of the new book" do
+      expect(@book.author).to eq("F. Scott Fitzgerald")
+    end
+
+    it "should save an empty array of rentals" do
+      expect(@book.rentals.size).to eq(0)
+    end
+  end
 end
