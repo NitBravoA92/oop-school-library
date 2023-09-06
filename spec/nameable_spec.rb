ba4618790ab78459fc526838ad8634108719be5e
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-07 00:50:42 +0300

    Nameable: added the test for correct_name method

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -10,4 +10,10 @@ describe Nameable do
       expect(@nameable).to be_instance_of(Nameable)
     end
   end
+
+  context "correct_name method" do
+    it "should return a message: 'NotImplementedError: correct_name is not implemented' when the name is not set" do
+      expect { @nameable.correct_name }.to raise_error(NotImplementedError)
+    end
+  end
 end
