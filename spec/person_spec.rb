84bf06c08d0befe94b87ee056bed5b51e2650523
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-06 19:10:15 -0400

    Test the instantiation of the Person class and display the class attributes

---
+++ spec/person_spec.rb
@@ -0,0 +1,25 @@
+require_relative '../classes/person'
+
+describe Person do
+  before :each do
+    @person = Person.new(1, 20, 'James Lenz', parent_permission: true)
+  end
+
+  describe "#new" do
+    it "should be able to be created" do
+      expect(@person).to be_instance_of(Person)
+    end
+
+    it "should save the id of the new person" do
+      expect(@person.id).to eq(1)
+    end
+
+    it "should save the age of the new person" do
+      expect(@person.age).to eq(20)
+    end
+
+    it "should save the name of the new person" do
+      expect(@person.name).to eq("James Lenz")
+    end
+  end
+end
