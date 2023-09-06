513e276b41118b914915e0cd2d590e8ef5830656
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-07 00:50:19 +0300

    Nameable: Added the test for creating new instance

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -4,4 +4,10 @@ describe Nameable do
   before :each do
     @nameable = Nameable.new
   end
+
+  describe "#new" do
+    it "should create a new Nameable object" do
+      expect(@nameable).to be_instance_of(Nameable)
+    end
+  end
 end
