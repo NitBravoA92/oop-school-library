b055b0d903fbdd1907f09ba7e97cbeadf6de3b58
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-06 19:37:16 -0400

    Test the attributes book and person of the Rental class

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -19,5 +19,15 @@ describe Rental do
     it "should save the date of the new rental" do
       expect(@rental.date).to eq("2023-08-31")
     end
+
+    it "should save an object of the Book class" do
+      expect(@rental.book).to be_instance_of(Book)
+      expect(@rental.book).to eq(@book)
+    end
+  
+    it "should save an object of the Teacher class" do
+      expect(@rental.person).to be_instance_of(Teacher)
+      expect(@rental.person).to eq(@teacher)
+    end
   end
 end
