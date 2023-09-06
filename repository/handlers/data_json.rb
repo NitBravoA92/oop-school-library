e90b7908a9925fa841f92a2e6a3405240cc74d86
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-05 21:47:33 -0400

    Create the find_all_people method to get all the people data from the file

--- repository/handlers/data_json.rb
+++ repository/handlers/data_json.rb
@@ -6,6 +6,23 @@ module DataRepository
     data.map { |book| Book.new(book["id"], book["title"], book["author"]) }
   end
 
+  def find_all_people
+    people_records = []
+    data = get_data('people.json')
+    data.each do |p|
+      person = nil
+      if p['type'] == 'student'
+        person = Student.new(p['id'], Classroom.new('Biology'), p['age'], p['name'], parent_permission: p['parent_permission'])
+        person.type = 'student'
+      else
+        person = Teacher.new(p['id'], p['specialization'], p['age'], p['name'])
+        person.type = 'teacher'
+      end
+      people_records << person
+    end
+    people_records
+  end
+
   def get_data(filename)
     file = "./repository/data/#{filename}"
     return [] unless File.exist?(file)
