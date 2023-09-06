d04c8d3b75159c559d058f0547a789f7b6348134
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-05 21:27:37 -0400

    Add the id parameter to the initialize method of the Student class

--- classes/student.rb
+++ classes/student.rb
@@ -3,13 +3,25 @@ require_relative 'person'
 class Student < Person
   attr_accessor :classroom
 
-  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(id, classroom, age, name = 'Unknown', parent_permission: true)
+    super(id, age, name, parent_permission: parent_permission)
     @classroom = classroom
     classroom.enter_student(self) # enter the student in the classroom when an student object is created
   end
 
   def play_hooky
     '¯(ツ)/¯'
+  end
+
+  def to_json
+    {
+      id: @id,
+      classroom: @classroom.label,
+      type: 'student',
+      name: @name,
+      age: @age,
+      parent_permission: @parent_permission,
+      rentals: @rentals.map { |r| r.id }
+    }
   end
 end
