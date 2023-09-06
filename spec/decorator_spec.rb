fecdcfe473efc33916154835d56423caa99d7271
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-07 00:52:12 +0300

    Decorator: Added the test for creating a new instance

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -6,4 +6,10 @@ describe Decorator do
     @person = Person.new(5, 17, "John", parent_permission: true) # Person inherits from Nameable
     @decorator = Decorator.new(@person)
   end
+
+  describe "#new" do
+    it "should create a new Decorator object" do
+      expect(@decorator).to be_instance_of(Decorator)
+    end
+  end
 end
