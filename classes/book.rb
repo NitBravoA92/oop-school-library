c9ca54a5ab780af817119724e08f6d985d50af7b
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-06 03:46:46 +0300

    Added the Id attribute the Book class

--- classes/book.rb
+++ classes/book.rb
@@ -1,7 +1,8 @@
 class Book
-  attr_accessor :title, :author, :rentals
+  attr_accessor :id, :title, :author, :rentals
 
-  def initialize(title, author)
+  def initialize(id, title, author)
+    @id = id
     @title = title
     @author = author
     @rentals = []
