e67c4eadbf36763bb6e9e6ee1522ed19d47b8489
ahmedeid6842 <ahmedeid6842@gmail.com>
2023-09-07 00:52:35 +0300

    Decorator: Added the test for correct_name method

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -12,4 +12,10 @@ describe Decorator do
       expect(@decorator).to be_instance_of(Decorator)
     end
   end
+
+  context "correct_name method" do
+    it "should return the the name of the person" do
+      expect(@decorator.correct_name).to eq("John")
+    end
+  end
 end
