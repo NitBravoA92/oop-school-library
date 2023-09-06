d628831e6eac53d0d35e5149e8e92fb973a75a25
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-05 21:21:34 -0400

    Create the get_data and insert_data methods to read and write the files

--- repository/handlers/data_json.rb
+++ repository/handlers/data_json.rb
@@ -5,4 +5,14 @@ module DataRepository
     data = get_data("books.json")
     data.map { |book| Book.new(book["id"], book["title"], book["author"]) }
   end
+
+  def get_data(filename)
+    file = "./repository/data/#{filename}"
+    return [] unless File.exist?(file)
+    JSON.parse(File.read(file))
+  end
+
+  def insert_data(filename, data)
+    File.write("./repository/data/#{filename}", JSON.pretty_generate(data))
+  end
 end
