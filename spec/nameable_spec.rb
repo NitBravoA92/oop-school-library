594c1b68f607df951599176170067cea5734b877
Carmen Bravo <nbravoalcala@gmail.com>
2023-09-06 20:08:03 -0400

    Fix some linters issues

--- spec/nameable_spec.rb
+++ spec/nameable_spec.rb
@@ -1,17 +1,17 @@
-require_relative "../classes/nameable"
+require_relative '../classes/nameable'
 
 describe Nameable do
   before :each do
     @nameable = Nameable.new
   end
 
-  describe "#new" do
-    it "should create a new Nameable object" do
+  describe '#new' do
+    it 'should create a new Nameable object' do
       expect(@nameable).to be_instance_of(Nameable)
     end
   end
 
-  context "correct_name method" do
+  context 'correct_name method' do
     it "should return a message: 'NotImplementedError: correct_name is not implemented' when the name is not set" do
       expect { @nameable.correct_name }.to raise_error(NotImplementedError)
     end
